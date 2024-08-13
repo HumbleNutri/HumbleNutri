7822d3b8fa12927d053a603f072c2791223e3670
HumbleNutri <seodongh@usc.edu>
2024-08-13 10:55:16 -0700

    find path

--- optimize_LP.py
+++ optimize_LP.py
@@ -173,7 +173,7 @@ def LP_MealBundle(bf_items, wg_items, vg_items, main_items, gender, height, weig
 
     # Solve the problem iteratively # Generate 10 bundles
     while len(bundles)< 50:
-        solver = pulp.PULP_CBC_CMD()
+        solver = pulp.PULP_CBC_CMD(solver_path)
         prob.solve(solver)
         if prob.status == pulp.LpStatusOptimal:
             # Create a bundle from the optimal solution
