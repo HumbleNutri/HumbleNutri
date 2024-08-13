1ec5acdc2d1186c654ed09879e99ca5e3da0c6b6
HumbleNutri <seodongh@usc.edu>
2024-08-12 21:16:01 -0700

    test

--- optimize_LP.py
+++ optimize_LP.py
@@ -3,11 +3,10 @@ import pulp
 from constraints import IBW_constraints
 from utils import torf
 
-#pulp.pulpTestAll()
-solver_list = pulp.listSolvers(onlyAvailable=True)
-print(solver_list)
-
 def LP_MealBundle(bf_items, wg_items, vg_items, main_items, gender, height, weight, age, after_surgery, activity_level, pre_diabetes, high_cholesterol, hypertension):
+    pulp.pulpTestAll()
+    solver_list = pulp.listSolvers(onlyAvailable=True)
+    print(solver_list)
     # set seed
     random.seed(2024)
     # Get constraints
