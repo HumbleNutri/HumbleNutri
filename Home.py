9dec1f4971cefd4a24d904a51c3ff3747a0e8989
HumbleNutri <seodongh@usc.edu>
2024-10-02 12:21:45 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -69,7 +69,7 @@ st.markdown(
         iii) a significant portion of their reviews were dedicated to Latin American cuisine. We consider the top 25% recommended items for each user as a candidate recipes. \
         Example user that was used in this prototype could be substitute with your patient, when your patient provide us with their recipe preference (e.g. ratings on recipes). \
         We then apply linear programming with objective function to maximize recommendation score from the recommender system to tailor the bundles to each user \
-        along with multiple constraints based on mock patient information (age, weight, health condition, etc.) that we assigned to each user to generate bundles from candidate items. \
+        along with multiple constraints based on patient information (age, weight, health condition, etc.) that we assigned to each user to generate bundles from candidate items. \
         Details of this approach is be also included in our paper.
     """
 )
